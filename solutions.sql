0ab3b63c6fa576ac40d19dfced70cfe01fcf85a2
Sabika Ali <Sabikaalghadeeri@gmail.com>
2022-10-12 14:11:53 +0300

    half answers

--- solutions.sql
+++ solutions.sql
@@ -1,12 +1,18 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+ SELECT subjects From subjects
 -- 2. Find all subjects sorted by location
+ SELECT location From subjects
 
 -- ### Where
 -- 3. Find the book "Little Women"
+SELECT * FROM books
+WHERE title = 'Little Women';
 -- 4. Find all books containing the word "Python"
+SELECT * FROM books WHERE title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+SELECT * FROM subjects
+WHERE location LIKE '%Main St%';
 
 -- ### Joins
 
